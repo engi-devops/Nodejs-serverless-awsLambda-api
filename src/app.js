0c83e4be279ac45b172db4439ebb6e8abf640559
santosh singh <singhsantosh.edu.in@gmail.com>
2021-03-19 23:33:27 +0530

    set environment

--- src/app.js
+++ src/app.js
@@ -20,7 +20,6 @@ const app = express();
 app.use(cors({ exposedHeaders: ['X-Token', 'X-Powered-By'] }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '50mb',}),)
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, }),)
 
