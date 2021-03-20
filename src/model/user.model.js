867476fbadd87b4abaf220cd5213f038cd6ccc21
santosh singh <singhsantosh.edu.in@gmail.com>
2021-03-20 09:42:46 +0530

    add validation or some error

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-    
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
+
     const UserItemSchema = new mongoose.Schema({
         name: {
             type: String,
             trim: true,
-            required: true,
+            required: [true, "name is required."]
         },
         email: {
             type: String,
             trim: true,
             lowercase: true,
-            required: true,
+            required: [true, "E-mail is required."],
             index: {
                 uniqe : true
             },
@@ -19,15 +21,33 @@ const mongoose = require('mongoose');
         },
         phone_number: {
             type: Number,
-            required: true,
+            required: [true, "phone number is required"]
         },
         password: {
             type: String,
-            required: true
+            required: [true, "password is required."]
         }
     }, {
         timestamps : true       
     });
 
-    
-module.exports = mongoose.model('user', UserItemSchema);
+    UserItemSchema.path('email').validate(async email => {
+        const emailCount = await mongoose.models.user.countDocuments({
+            email
+        })
+        return !emailCount;
+
+    }, "E-mail already exists.");
+
+    UserItemSchema.pre('save', function (next) {
+        if (!this.isModified('password')) return next();
+        bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash
+            next();
+        })
+    })
+
+const userModel = mongoose.model('user', UserItemSchema);
+
+module.exports = userModel;
